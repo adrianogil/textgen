dd912063a661628ab08e5deb1a573717e21a55c0
Adriano Gil <adrianogil.san@gmail.com>
2022-08-27 10:35:01 -0400

    Added phrases with "used to"

--- src/python/textgen/english/gen_english_phrase.py
+++ src/python/textgen/english/gen_english_phrase.py
@@ -5,7 +5,8 @@ def gen_english_phrase():
     english_phrase = SimpleGrammar()\
         .set_text("#main_structure#")\
         .add_tag("main_structure", [
-            "#unreal_conditionals#"
+            "#unreal_conditionals#",
+            "#used_to_phrases#"
         ])\
         .add_tag("unreal_conditionals", [
             "If there was a fire in this hotel, it would be very difficult to escape.",
@@ -13,14 +14,28 @@ def gen_english_phrase():
             "If it wasn't raining so hard, we could get to #seemingly_unreachable_place#.",
             "If I were you, I'd make #random_person_name# wear a helmet when he's riding a bike"
             ])\
+        .add_tag("used_to_phrases", [
+            "#my_animals# are not used to #thing_that_scare_animals#, they always get scared"
+            ])\
         .add_tag("seemingly_unreachable_place", [
             "the end of this road",
             "the top of the mountain",
             ])\
+        .add_tag("thing_that_scare_animals", [
+            "listening to the ships",
+            "listening to church bell ringing",
+            ])\
         .add_tag("random_person_name", [
             "Jimmy",
             "Samantha"
+            ])\
+        .add_tag("my_animals", [
+            "my cats",
+            "my fishes",
+            "my dogs"
             ])
+
+        # My cats are not used to listening to the ships, they always get scared
 
     print(str(english_phrase))
 
